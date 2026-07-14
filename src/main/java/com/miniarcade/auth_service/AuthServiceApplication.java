76f112ae096b550de9ed0848701761f88fb559fc
Urgyán Bálint Viktor <urgyanbalintviktor@gmail.com>
2026-07-14 19:50:00 +0200

    feat: Fine tuning backend

--- src/main/java/com/miniarcade/auth_service/AuthServiceApplication.java
+++ src/main/java/com/miniarcade/auth_service/AuthServiceApplication.java
@@ -2,12 +2,12 @@ package com.miniarcade.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class AuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
 }
--- src/main/java/com/miniarcade/auth_service/security/JwtAuthenticationFilter.java
+++ src/main/java/com/miniarcade/auth_service/security/JwtAuthenticationFilter.java
@@ -1,13 +1,11 @@
 package com.miniarcade.auth_service.security;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.lang.NonNull;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
